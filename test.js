7a24bb205c2adf901806b07727e151099365f748
ShadeF <samanthashade83@gmail.com>
2023-08-02 19:17:41 +0300

    Frontend form, test.js to connect to frontend

---
+++ test.js
@@ -0,0 +1,16 @@
+//this allows you to redirect to home.hbs
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.set('views', 'views')
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.get ('/', function(request, response){
+    response.render('home');
+})
+
+
+app.listen(port)
+console.log("Heyyy Node server started on port 3000")
