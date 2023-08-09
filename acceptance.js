85fa73c8a5052337a9729b6bc0a6c07f778bb488
ilovecode100 <muthomirichard@hotmail.com>
2023-08-09 11:42:44 +0300

    acceptance test added

---
+++ acceptance.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const { remote } = require('webdriverio');
+
+(async () => {
+        browser = await remote({
+        capabilities: { browserName: 'edge' }
+        
+    });
+
+    try {
+        // Open the lecturer rating website
+        await browser.navigateTo('https://lec-review-tyj8.onrender.com/');
+
+        // Enter name
+        const nameInput = await browser.$('#name');
+        await nameInput.setValue('John Doe');
+
+        // Select rating out of 10
+        const ratingInput = await browser.$('[name="rating"]');
+        await ratingInput.selectByVisibleText('8'); // Assuming 8 out of 10
+
+        // Submit the form
+        const submitButton = await browser.$('[type="submit"]');
+        await submitButton.click();
+
+        // Verify report content
+        const report = await browser.$('.report');
+        const reportText = await report.getText();
+
+        // Assuming the report displays the submitted data
+        assert.strictEqual(reportText.includes('Name: John Doe'), true);
+        assert.strictEqual(reportText.includes('Rating: 8'), true);
+        assert.strictEqual(reportText.includes('Report Generated'), true);
+    } finally {
+        await browser.deleteSession();
+    }
+})();
